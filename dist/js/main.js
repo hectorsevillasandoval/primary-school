62c35ceab9bb9b14c846e82f169b9e680c9a7d0d
Hector Sevilla <h2d_07@hotmail.com>
2017-01-19 21:39:13 -0600

    single gallery has been created

--- dist/js/main.js
+++ dist/js/main.js
@@ -12,5 +12,31 @@ jQuery(function($){
         return false;
     });
 
+    $(window).load(function() { 
+
+
+
+
+		$('#carousel').flexslider({
+	        animation: "slide",
+	        controlNav: false,
+	        animationLoop: false,
+	        slideshow: false,
+	        itemWidth: 210,
+	        itemMargin: 5,
+	        asNavFor: '#slider'
+	      });
+
+	      $('#slider').flexslider({
+	        animation: "slide",
+	        controlNav: false,
+	        animationLoop: false,
+	        slideshow: false,
+	        sync: "#carousel",
+	        start: function(slider){
+	          $('body').removeClass('loading');
+	        }
+	      });
+      });
 
 });
